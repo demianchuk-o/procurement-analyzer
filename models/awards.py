50e63cbbd320ed43394df18626a457925f16cac3
demianchuk-o <demyanchukua@gmail.com>
2025-03-20 13:42:19 +0200

    entities related to tender, with change tracking where necessary

---
+++ models/awards.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, String, DateTime, ForeignKey, Numeric, Text, Integer
+from sqlalchemy.orm import relationship
+
+from db import db
+
+
+class Award(db.Model):
+    __tablename__ = 'awards'
+
+    id = Column(String(32), primary_key=True)
+    tender_id = Column(String(32), ForeignKey('tenders.id'), nullable=False)
+    bid_id = Column(String(32), ForeignKey('bids.id'), nullable = False)
+    status = Column(String)
+    title = Column(String)
+    value_amount = Column(Numeric(18, 2))
+    award_date = Column(DateTime)
+
+    # Relationships
+    tender = relationship("Tender", back_populates="awards")
+    bid = relationship("Bid", back_populates="award")
+
+
+class AwardChange(db.Model):
+    __tablename__ = 'award_changes'
+
+    id = Column(Integer, primary_key=True)
+    award_id = Column(String(32), ForeignKey('awards.id'), nullable=False)
+    tender_id = Column(String(32), ForeignKey('tenders.id'), nullable=False)
+    bid_id = Column(String(32), ForeignKey('bids.id'), nullable=False)
+    change_date = Column(DateTime(timezone=True), nullable=False)
+    field_name = Column(String(50), nullable=False)
+    old_value = Column(String)
+    new_value = Column(String)
+    change_type = Column(String(50), nullable=False)
+
+    # Relationships
+    award = db.relationship("Award", back_populates="changes")
---
+++ models/complaints.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, ForeignKey, Text, DateTime, String, JSON
+from sqlalchemy.orm import relationship
+
+from db import db
+
+
+class Complaint(db.Model):
+    __tablename__ = 'complaints'
+
+    id = Column(String(32), primary_key=True)
+    tender_id = Column(String(32), ForeignKey('tenders.id'), nullable=False)
+    status = Column(String)
+    title = Column(Text)
+    description = Column(Text)
+    date = Column(DateTime)
+    date_submitted = Column(DateTime)
+    date_answered = Column(DateTime)
+    type = Column(String(50), nullable=False)
+    highlighted_keywords = Column(JSON)
+
+    # Relationships
+    tender = db.relationship("Tender", back_populates="complaints")
+    changes = db.relationship("ComplaintChange", back_populates="complaint",
+                              cascade="all, delete-orphan")
+
+class ComplaintChange(db.Model):
+    __tablename__ = 'complaint_changes'
+
+    id = Column(Integer, primary_key=True)
+    complaint_id = Column(String(32), ForeignKey('complaints.id'), nullable=False)
+    tender_id = Column(String(32), ForeignKey('tenders.id'), nullable=False)
+    change_date = Column(DateTime(timezone=True), nullable=False)
+    field_name = Column(String(50), nullable=False)
+    old_value = Column(String)
+    new_value = Column(String)
+    change_type = Column(String(50), nullable=False)
+
+    #Relationships
+    complaint = db.relationship("Complaint", back_populates="changes")
