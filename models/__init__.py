ed54bd2fb7a69d3041532af91cd7350b7a5f933f
demianchuk-o <demyanchukua@gmail.com>
2025-03-28 17:26:48 +0200

    update models init

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@ from .tender_documents import TenderDocument, TenderDocumentChange
 from .awards import Award, AwardChange
 from .bids import Bid, BidChange
 from .complaints import Complaint, ComplaintChange
-from .item_classifications import ItemClassification
+from .general_classifiers import GeneralClassifier
 from .users import User
 from .user_subscriptions import UserSubscription
 from .violation_scores import ViolationScore
