0e5beb737854e5d3b74370dfcf02778e1033663b
demianchuk-o <demyanchukua@gmail.com>
2025-04-27 20:37:27 +0300

    removed new entries query for redundancy reasons

--- repositories/change_repository.py
+++ repositories/change_repository.py
@@ -3,7 +3,7 @@ from typing import List
 
 from sqlalchemy.orm import Session
 
-from models.typing import ChangeT, ChildEntityT
+from models.typing import ChangeT
 from repositories.base_datasource import BaseDatasource
 
 
@@ -26,21 +26,4 @@ class ChangeRepository(BaseDatasource):
         return self._session.query(change_cls).filter(
             change_cls.tender_id == tender_id,
             change_cls.change_date >= since_date
-        ).all()
-
-    def get_new_entries_since(self, entity_cls: ChildEntityT, tender_id: str, since_date: datetime) -> List:
-        """
-        Retrieves new entries for a specific tender since a given date.
-
-        Args:
-            entity_cls: The SQLAlchemy model class for the entity table (e.g., TenderDocument, Bid).
-            tender_id: The ID of the tender to filter new entries for.
-            since_date: The datetime object representing the start of the time range.
-
-        Returns:
-            A list of new entry records (instances of the model_cls) for the specified tender and date range.
-        """
-        return self._session.query(entity_cls).filter(
-            entity_cls.tender_id == tender_id,
-            entity_cls.created_at >= since_date
         ).all()
