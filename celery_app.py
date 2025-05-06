80e95a180e87eda5c9c73e74a1ce78a5ef495ca2
demianchuk-o <demyanchukua@gmail.com>
2025-05-06 16:59:11 +0300

    Add celery beat tasks

--- celery_app.py
+++ celery_app.py
@@ -1,8 +1,23 @@
 from celery import Celery
+from celery.schedules import crontab
 
-
-app = Celery('tender_complaints')  # Replace 'tender_complaints' with your project name
+app = Celery('tender_tasks')
 app.config_from_object('celeryconfig')
+
+app.conf.beat_schedule = {
+    'crawl-tenders-every-15-minutes': {
+        'task': 'tasks.crawl_tenders_task',
+        'schedule': crontab(minute='*/15'),
+    },
+    'sync-all-tenders-every-hour': {
+        'task': 'tasks.sync_all_tenders_task',
+        'schedule': crontab(minute=0, hour='*/1'),
+    },
+    'send-notifications-every-hour': {
+        'task': 'tasks.send_notifications_task',
+        'schedule': crontab(minute=0, hour='*/1'),
+    },
+}
 
 if __name__ == '__main__':
     app.start()
---
+++ tasks.py
@@ -0,0 +1,49 @@
+from celery_app import app
+from repositories.tender_repository import TenderRepository
+from services.crawler_service import CrawlerService
+from services.datetime_provider import DatetimeProvider
+from services.email_service import EmailService
+from services.html_report_builder import HtmlReportBuilder
+from services.notification_service import NotificationService
+from db import db
+from services.report_generation_service import ReportGenerationService
+
+def create_tender_repository():
+    """Create a TenderRepository instance."""
+    return TenderRepository(db.session)
+
+def create_report_builder():
+    """Create a ReportBuilder instance."""
+    return ReportGenerationService(db.session)
+
+@app.task(name='tasks.crawl_tenders_task')
+def crawl_tenders_task():
+    with app.app_context():
+        tender_repository = create_tender_repository()
+        crawler_service = CrawlerService(tender_repository)
+        crawler_service.crawl_tenders(pages_to_crawl=1)
+
+@app.task(name='tasks.sync_all_tenders_task')
+def sync_all_tenders_task():
+    with app.app_context():
+        tender_repository = create_tender_repository()
+        crawler_service = CrawlerService(tender_repository)
+        crawler_service.sync_all_tenders()
+
+@app.task(name='tasks.send_notifications_task')
+def send_notifications_task():
+    with app.app_context():
+        notification_service = NotificationService(
+            tender_repository=create_tender_repository(),
+            report_generator=create_report_builder(),
+            html_builder=HtmlReportBuilder(),
+            email_service=EmailService(
+                smtp_server=app.config['SMTP_SERVER'],
+                port=app.config['SMTP_PORT'],
+                sender_email=app.config['SMTP_USER'],
+                password=app.config['SMTP_PASSWORD']
+            ),
+            datetime_provider=DatetimeProvider(),
+            report_interval_hours=1
+        )
+        notification_service.send_notifications()
