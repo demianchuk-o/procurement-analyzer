ede47c454ca25c83437ca4062f4dec57e1336792
demianchuk-o <demyanchukua@gmail.com>
2025-05-08 10:54:37 +0300

    simplify complaint keywords embedding

--- util/complaint_text_render.py
+++ util/complaint_text_render.py
@@ -1,26 +1,15 @@
 def process_complaint_text(text, complaint_keywords, keyword_field_map):
-    processed_text = text
-    offset = 0
-    for keyword_data in complaint_keywords:
-        keyword = keyword_data["keyword"]
-        domains = keyword_data["domains"]
-        start = keyword_data["startPosition"]
-        length = keyword_data["length"]
+    for kw in sorted(complaint_keywords, key=lambda x: x["startPosition"], reverse=True):
+        start = kw["startPosition"]
+        length = kw["length"]
         end = start + length
 
-        domain_names = [keyword_field_map.get(str(domain), "Unknown") for domain in domains]
-        domains_str = ", ".join(domain_names)
-
-        replacement_text = (
-            f'<b>{keyword}</b>'
+        domains = [keyword_field_map.get(str(d), "Unknown") for d in kw["domains"]]
+        domains_str = ", ".join(domains)
+        replacement = (
+            f'<b>{kw["keyword"]}</b>'
             f'<br><span style="color: blue;">({domains_str})</span><br>'
         )
 
-        new_start = start + offset
-        new_end = end + offset
-        processed_text = (
-            processed_text[:new_start] + replacement_text + processed_text[new_end:]
-        )
-        offset += len(replacement_text) - length
-
-    return processed_text
+        text = text[:start] + replacement + text[end:]
+    return text
