dde7c83d532031cc7658ad015ec6350d6a4958db
demianchuk-o <demyanchukua@gmail.com>
2025-05-08 12:33:35 +0300

    improve keyword usability with tooltips

--- util/complaint_text_render.py
+++ util/complaint_text_render.py
@@ -1,19 +1,17 @@
-import json
-
-
 def process_complaint_text(text, complaint_keywords, keyword_field_map):
     for kw in sorted(complaint_keywords, key=lambda x: x["startPosition"], reverse=True):
-        start = kw["startPosition"]
-        length = kw["length"]
+        start, length = kw["startPosition"], kw["length"]
         end = start + length
-
         domains = [keyword_field_map.get(str(d), "Unknown") for d in kw["domains"]]
         domains_str = ", ".join(domains)
         replacement = (
-            f'<b>{kw["keyword"]}</b>'
-            f'<br><span style="color: blue;">({domains_str})</span><br>'
+            f'<strong '
+            f'class="complaint-keyword" '
+            f'data-bs-toggle="tooltip" '
+            f'title="{domains_str}">'
+            f'{kw["keyword"]}'
+            f'</strong>'
         )
-
         text = text[:start] + replacement + text[end:]
     return text
 
