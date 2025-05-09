374d80d34de3925a89109037f34cdb87a6f91688
demianchuk-o <demyanchukua@gmail.com>
2025-05-09 15:22:12 +0300

    extract original word as dictionary contains lemma now

--- util/complaint_text_render.py
+++ util/complaint_text_render.py
@@ -1,15 +1,17 @@
 def process_complaint_text(text, complaint_keywords, keyword_field_map):
+    original_text = text
     for kw in sorted(complaint_keywords, key=lambda x: x["startPosition"], reverse=True):
         start, length = kw["startPosition"], kw["length"]
         end = start + length
         domains = [keyword_field_map.get(str(d), "Unknown") for d in kw["domains"]]
         domains_str = ", ".join(domains)
+        actual_word = original_text[start:end]
         replacement = (
             f'<strong '
             f'class="complaint-keyword" '
             f'data-bs-toggle="tooltip" '
             f'title="{domains_str}">'
-            f'{kw["keyword"]}'
+            f'{actual_word}'
             f'</strong>'
         )
         text = text[:start] + replacement + text[end:]
