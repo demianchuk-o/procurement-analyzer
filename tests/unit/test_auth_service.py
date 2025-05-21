a94991f0445fd395c6ec17067f32737a77f53034
demianchuk-o <demyanchukua@gmail.com>
2025-05-21 12:25:47 +0300

    update auth service tests

--- tests/unit/test_auth_service.py
+++ tests/unit/test_auth_service.py
@@ -1,12 +1,13 @@
 import pytest
-from flask import Flask
-from unittest.mock import MagicMock
+from flask import Flask, redirect, url_for
+from unittest.mock import MagicMock, patch
 from services.auth_service import AuthService
 from repositories.user_repository import UserRepository
 
 
 class MockUser:
-    def __init__(self, email, password):
+    def __init__(self, id, email, password):
+        self.id = id
         self.email = email
         self._password_hash = password
 
@@ -23,6 +24,12 @@ class TestAuthService:
         app.config['JWT_SECRET_KEY'] = 'test_secret'
         app.config['JWT_TOKEN_LOCATION'] = ['cookies']
         app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+        app.config['SERVER_NAME'] = 'localhost.test'
+
+        @app.route('/')
+        def index():
+            return "OK"
+
         return app
 
     @pytest.fixture
@@ -40,12 +47,12 @@ class TestAuthService:
         return AuthService(app=app, user_repository=mock_user_repository, password_service=mock_password_service)
 
     @pytest.fixture(params=[
-        ("test@example.com", "password123"),
-        ("test2@example.com", "another_password")
+        (1, "test@example.com", "password123"),
+        (2, "test2@example.com", "another_password")
     ])
     def mock_user(self, request):
-        email, password = request.param
-        return MockUser(email, password)
+        id, email, password = request.param
+        return MockUser(id, email, password)
 
     def test_register_user_success(self, auth_service, mock_user_repository):
         """Test successful user registration."""
@@ -76,24 +83,26 @@ class TestAuthService:
 
         mock_user_repository.get_by_email.assert_called_once_with(email)
 
-
-    def test_login_success(self, app, auth_service, mock_user_repository, mock_user):
+    @patch('flask.redirect')
+    @patch('flask.url_for')
+    def test_login_success(self, mock_url_for, mock_redirect, app, auth_service, mock_user_repository, mock_user,
+                           mock_password_service):
         """Test successful login."""
         # Arrange
         email = mock_user.email
-        password = mock_user._password_hash
+        password = "password123"
         mock_user_repository.get_by_email.return_value = mock_user
+        mock_password_service.check_password.return_value = True
+        mock_url_for.return_value = '/index'
+        mock_redirect.return_value = MagicMock()
 
         # Act
         with app.app_context():
             result = auth_service.login(email, password)
 
         # Assert
-        assert result is not None
-        access_token, user = result
         mock_user_repository.get_by_email.assert_called_once_with(email)
-        assert access_token is not None
-        assert user == mock_user
+        mock_password_service.check_password.assert_called_once_with(mock_user.password_hash, password)
 
     def test_login_invalid_credentials(self, auth_service, mock_user_repository):
         """Test login with invalid credentials (user not found)."""
@@ -114,7 +123,7 @@ class TestAuthService:
         # Arrange
         email = "test@example.com"
         password = "wrong_password"
-        mock_user = MockUser(email, 'password123')
+        mock_user = MockUser(35, email, 'password123')
         mock_user_repository.get_by_email.return_value = mock_user
 
         auth_service.password_service.check_password.return_value = False
